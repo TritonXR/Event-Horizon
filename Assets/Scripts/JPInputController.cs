2db6cc8052411c031fb4baa5eca9cc7c4c41500b
Justin Park <justindypark@yahoo.com>
2017-11-10 15:57:10 -0800

    Added networking code, local player prefabs

---
+++ Assets/Scripts/JPInputController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class JPInputController : NetworkBehaviour {
+	JPNetworkPlayer networkPlayer;
+	GameObject selectedShip;
+	GameObject targetShip;
+	Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+		if(isLocalPlayer) {
+			networkPlayer = this.GetComponent<JPNetworkPlayer> ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isLocalPlayer) {
+
+		}
+	}
+	void selectShip (GameObject ship) {
+		networkPlayer.CmdSelectShip (ship.name);
+	}
+	void setTargetShip (GameObject ship) {
+		networkPlayer.CmdSetTargetShip (ship.name);
+	}
+	void setTargetPosition (Vector3 pos) {
+		networkPlayer.CmdSetPosition (pos);
+	}
+
+}
---
+++ Assets/Scripts/JPNetworkPlayer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class JPNetworkPlayer : MonoBehaviour {
+	GameObject selectedShip;
+	GameObject targetShip;
+	Vector3 targetPosition;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	[Command]
+	public void CmdSelectShip (string shipName) {
+		selectedShip = GameObject.Find (shipName);
+	}
+	[Command]
+	public void CmdSetTargetShip (string shipName) {
+		targetShip = GameObject.Find (shipName);
+		//selectedShip.GetComponent<OPShipMovement>().setTargetShip(targetShip);
+	}
+	[Command]
+	public void CmdSetPosition (Vector3 pos) {
+		targetPosition = pos;
+		//selectedShip.GetComponent<OPShipMovement>().setTargetPosition(pos);
+	}
+}
