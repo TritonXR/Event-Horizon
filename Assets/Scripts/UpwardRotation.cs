6f4ee68b311ae4848827b2b5593fbdbfe7186aef
Justin Park <justindypark@yahoo.com>
2018-06-06 17:35:44 -0700

    stickGrab updated

---
+++ Assets/Scripts/UpwardRotation.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Networking;
+
+public class UpwardRotation : MonoBehaviour {
+	public GameObject colliderCenter;
+	public Quaternion rotation;
+	void onTriggerEnter(Collider colliderCenter){
+		rotation = Quaternion.Euler(0, 10, 0);
+	}
+
+}
--- Assets/vr scripts/stickGrab.cs
+++ Assets/vr scripts/stickGrab.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class stickGrab : MonoBehaviour {
 	Vector3 initPos;
+    Vector3 startPos;       // Origin is not 0,0,0, but rather its own position. Needs to be factored in
 	Transform ship;
 	public bool throttle; 	// If throttle is checked, then only moves forward and backward. 
 							// Else, 360 rotation
@@ -11,16 +12,23 @@ public class stickGrab : MonoBehaviour {
 	float xMax = 10.0f;
 	float zMax = 10.0f;
 
-	// WHY IS THIS PUBLIC AND WHY IS IT BUGGY WHEN ITS PRIVATE
-	public bool used; // True, invis ball is being interacted/grabbed.
+    Quaternion startRotation;
+    Quaternion endRotation;
+    float rotationProgress = -1;
+
+    // WHY IS THIS PUBLIC AND WHY IS IT BUGGY WHEN ITS PRIVATE
+    public bool used; // True, invis ball is being interacted/grabbed.
 	Rigidbody rb;
 	// Use this for initialization
 	void Start () {
 		initPos = transform.localPosition; // When values are changed, need this to visually see ship moving.
-		ship = transform.parent.parent;
+        ship = transform.parent.parent;
 		used = false;
 		rb = ship.GetComponent<Rigidbody> ();
-	}
+
+   
+        Debug.Log("Starting Coordinates: (" + initPos.x + "," + initPos.y + "," + initPos.z+ ")");
+    }
 
 	public Vector3 getInitPos() {
 		return initPos;
@@ -50,65 +58,107 @@ public class stickGrab : MonoBehaviour {
 		// Movement rate and rotation
 		if (used == true) {
 			Vector3 localPos = transform.localPosition;
-			/* Difference between current localPosition and initial localPosition */
-			float xDistance = localPos.x - initPos.x;
+            /* Difference between current localPosition and initial localPosition */
+            float xDistance = localPos.x - initPos.x;
 			float yDistance = localPos.y - initPos.y;
 			float zDistance = localPos.z - initPos.z;
 
-			/* Debug */
-			/*print ("xDistance: " + xDistance + ". yDistance: " + yDistance + 
+            float deadZone = 2.0f;
+
+            Debug.Log("Distance: (" + xDistance + "," + yDistance + "," + zDistance + ")");
+
+            /* Debug */
+            /*print ("xDistance: " + xDistance + ". yDistance: " + yDistance + 
 				", zDistance: " + zDistance);*/
 
-			/* Throttle is checked off: 360 rotation instead of forward and backward */
-			if (throttle == false) {
+            /* Throttle is checked off: 360 rotation instead of forward and backward */
+            if (throttle == false) {
 				// Seperate movement and rotation to respective pair of quadrants 
-				float ratioXtoZ = localPos.x / localPos.z;
+				float ratioXtoZ = xDistance / zDistance;
 				//Debug.Log ("RatioXToZ: " + ratioXtoZ);
-				Debug.Log("localPos.x: " + localPos.x + ", localPos.z: " + localPos.z);
-
+           
 				// Top quadrant: Dive down 
-				if ( localPos.x > 0 /*&& localPos.x <= xMax*/ ) {
-					
+				if ( zDistance > deadZone ) {
 					// Left half 
-					if (localPos.z > 0 && ratioXtoZ > 1) {
-						// Rotate around z axis
-						Vector3 newVec = new Vector3 (0, 0, -1.0f * zDistance);
-						ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+					if (xDistance < 0 && ratioXtoZ > -1) {
+                        // Rotate around z axis
+                        Vector3 newVec = new Vector3(-1.0f * zDistance, 0, 0);
+                        ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
 					}
 
-					// Right half 
-					else if (localPos.z < 0 && ratioXtoZ < -1) {
-						Vector3 newVec = new Vector3 (0, 0, -1.0f * zDistance);
-						ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    // Right half 
+                    else  if (xDistance > 0 && ratioXtoZ < 1) {
+                        Vector3 newVec = new Vector3(-1.0f * zDistance, 0, 0);
+                        ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
 					}
-				}
-				/*
-				// Bottom quadrant: Dive up
-				if ( localPos.x < 0 && localPos.x >= xMax ) {
-					// Left half
-					if (localPos.z > 0 && ratioXtoZ > -1) {
-						Vector3 newVec = new Vector3 (0, 0, 1.0f * zDistance);
-						ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
-					}
-					// Right half 
-					else if (localPos.z < 0 && ratioXtoZ < 1) {
-						Vector3 newVec = new Vector3 (0, 0, 1.0f * zDistance);
-						ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
-					}
-				}*/
+                }
 
-				// Movement 
-				//Vector3 newVec = new Vector3 (
-				//	1.0f * xDistance, 1.0f * yDistance, 1.0f * zDistance);
+                // Bottom quadrant: Dive up
+                if (zDistance < -1 * deadZone) {
+                    // Left half 
+                    if (xDistance < 0 && ratioXtoZ < 1) {
+                        // Rotate around z axis
+                        Vector3 newVec = new Vector3(-1.0f * zDistance, 0, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
+                    
+                    // Right half 
+                    else if (xDistance > 0 && ratioXtoZ > -1)
+                    {
+                        Vector3 newVec = new Vector3(-1.0f * zDistance, 0, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
+                }
+                
+                // Left Quadrant: Yaw Left
+                if (xDistance < -1 * deadZone)
+                {
+                    // Top half 
+                    if (zDistance > 0 && ratioXtoZ < -1)
+                    {
+                        // Rotate around y axis
+                        Vector3 newVec = new Vector3(0, 1.0f * xDistance, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
 
-				// Rotation 
-				//ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
-			} else {
+                    // Bottom half 
+                    else if (zDistance < 0 && ratioXtoZ > 1)
+                    {
+                        Vector3 newVec = new Vector3(0, 1.0f * xDistance, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
+                }
+                // Right Quadrant: Yaw Right
+                if (xDistance > deadZone)
+                {
+                    // Top half 
+                    if (zDistance > 0 && ratioXtoZ > 1)
+                    {
+                        // Rotate around y axis
+                        Vector3 newVec = new Vector3(0, 1.0f * xDistance, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
+
+                    // Bottom half 
+                    else if (zDistance < 0 && ratioXtoZ < -1)
+                    {
+                        Vector3 newVec = new Vector3(0, 1.0f * xDistance, 0);
+                        ship.Rotate(newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+                    }
+                }
+
+                // Movement 
+                //Vector3 newVec = new Vector3 (
+                //	1.0f * xDistance, 1.0f * yDistance, 1.0f * zDistance);
+
+                // Rotation 
+                //ship.Rotate (newVec * 6.0f * rotationsPerMinute * Time.deltaTime);
+            } else {
 				
 				if (rb.velocity.magnitude >= 25.0f) {
-					rb.velocity = ship.right * zDistance * 10;
+					rb.velocity = ship.forward* zDistance * 10;
 				} else {
-					rb.AddForce(ship.right * zDistance * 25);
+					rb.AddForce(ship.forward * zDistance * 25);
 				}
 
 			}
@@ -118,6 +168,75 @@ public class stickGrab : MonoBehaviour {
 		} else {
 			/* Set initial localPosition to current localPosition */
 			transform.localPosition = initPos;
-		}
+
+            Debug.Log("Angles: " + ship.rotation.x +","+ ship.rotation.y + ","+ ship.rotation.z);
+
+
+            StartRotating(0.0f);
+
+            if (rotationProgress < 1 && rotationProgress >= 0)
+            {
+                Debug.Log("Its printing");
+                rotationProgress += Time.deltaTime * 5;
+
+                // Here we assign the interpolated rotation to transform.rotation
+                // It will range from startRotation (rotationProgress == 0) to endRotation (rotationProgress >= 1)
+                transform.rotation = Quaternion.Lerp(startRotation, endRotation, rotationProgress);
+            }
+
+
+
+
+            /*
+            if (ship == null)
+            {
+                StartCoroutine(RotateImage());
+            }
+            */
+
+        }
 	}
+
+    // Call this to start the rotation
+    void StartRotating(float zPosition)
+    {
+
+        // Here we cache the starting and target rotations
+        startRotation = transform.rotation;
+        endRotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, zPosition);
+
+        // This starts the rotation, but you can use a boolean flag if it's clearer for you
+        rotationProgress = 0;
+    }
+
+    
+
+
+    IEnumerator RotateImage()
+    {
+        /*
+        float moveSpeed = 0.1f;
+        while (ship.transform.rotation.y < 180)
+        {
+            ship.transform.rotation = Quaternion.Lerp(ship.transform.rotation, Quaternion.Euler(0, 180, 0), moveSpeed * Time.time);
+            yield return null;
+        }
+        ship.transform.rotation = Quaternion.Euler(0, 180, 0);
+        yield return null;
+        */
+        
+        float moveSpeed = 0.1f;
+        if (used != true) {
+            yield return null;
+        }
+        while (rb.transform.rotation.z >= -90 && rb.transform.rotation.z <= 90)
+        {
+            rb.transform.rotation = Quaternion.Slerp(rb.transform.rotation, 
+                Quaternion.Euler(rb.transform.rotation.x, rb.transform.rotation.y, 0), moveSpeed * Time.time);
+            yield return null;
+        }
+        rb.transform.rotation = Quaternion.Euler(rb.transform.rotation.x, rb.transform.rotation.y, 0);
+        yield return null;
+
+    }
 }
